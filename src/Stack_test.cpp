81972a04b39b11290f2e679037e65d81633d0a33
void404 <wojciech.nawa@gmail.com>
2022-04-29 16:40:29 +0200

    Write tests

--- src/Stack_test.cpp
+++ src/Stack_test.cpp
@@ -1,5 +1,9 @@
 #include "Stack.h"
 #include "ut.hpp"
+#include <sstream>
+
+using std::ostringstream;
+using std::string;
 
 int main() {
   using namespace boost::ut;
@@ -7,6 +11,13 @@ int main() {
 
   describe("Stack") = [] {
     it("has a constructor") = [] { expect(nothrow([]() { Stack<int>(); })); };
+    it("can be copied") = [] {
+      Stack<int> s1(10, 7);
+      Stack<int> s2 = s1;
+      expect(that % s1.size() == s2.size());
+      expect(that % s2.pop() == 7);
+      expect(that % s1.size() == s2.size() + 1);
+    };
 
     it("can push and pop items") = [] {
       Stack<int> a;
@@ -17,6 +28,10 @@ int main() {
       expect(that % a.size() == 7);
       a.push(7);
       expect(that % a.size() == 8);
+
+      expect(that % a.pop() == 7);
+      expect(that % a.pop() == 6);
+      expect(that % a.size() == 6);
     };
 
     it("has state") = [] {
@@ -24,6 +39,22 @@ int main() {
       expect(that % a.empty());
       a.push(0);
       expect(that % not a.empty());
+    };
+
+    it("can be printed") = [] {
+      Stack<uint> s;
+
+      for (auto i = 0; i < 5; i++) {
+        s.push(i);
+      }
+
+      ostringstream stream;
+      stream << s;
+
+      string actual = stream.str();
+      string expected = "[4 3 2 1 0]";
+
+      expect(that % actual == expected);
     };
   };
 
